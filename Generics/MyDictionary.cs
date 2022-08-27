427f3b0a9ec73bdabc3a0a91d7ae77213f1262b6
erdemmimet@gmail.com <erdemmimet@gmail.com>
2022-08-27 08:42:10 +0300

    Dictionary

---
+++ Generics/MyDictionary.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Generics
+{
+    class MyDictionary<T,K>
+    {
+        KeyValuePair<T,K>[] items;
+
+       public MyDictionary()
+        {
+            items = new KeyValuePair<T, K>[0];
+        }
+
+        public void Add(T key, K value)
+        {
+        
+            if (Control(key))
+            {
+                KeyValuePair<T, K>[] tempArray = items;
+                items = new KeyValuePair<T, K>[items.Count() + 1];
+
+                for (int i = 0; i < tempArray.Length; i++)
+                {
+                    items[i] = tempArray[i];
+                }
+                items[items.Count() - 1] = new KeyValuePair<T, K>(key, value);
+            }
+            else
+            {
+                Console.WriteLine("Bu kayıt daha önceden eklenmiştir");
+            }
+            
+        }
+
+        public void List()
+        {
+            foreach (var item in items)
+            {
+                Console.WriteLine(item.Key+" "+item.Value);
+            }
+        }
+
+        public bool Control(T key)
+        {
+            bool control = true;
+            foreach (var item in items)
+            {
+
+                if (item.Key.ToString()==key.ToString())
+                {
+
+                     control = false;
+
+                }
+                
+            }
+            return control;
+        }
+       
+
+        public int Count
+        {
+            get { return items.Count(); }
+          
+        }
+
+    }
+}
--- Generics/Program.cs
+++ Generics/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace Generics
 {
@@ -6,11 +7,29 @@ namespace Generics
     {
         static void Main(string[] args)
         {
-            MyList<string> myList = new MyList<string>();
-            myList.Add("Gamze");
-            myList.Add("Mehmet");
-            Console.WriteLine(myList.Count);
-            
+            //MyList<string> myList = new MyList<string>();
+            //myList.Add("Gamze");
+            //myList.Add("Mehmet");
+            //Console.WriteLine(myList.Count);
+
+
+            //Dictionary<int, string> keyValues = new Dictionary<int, string>();
+            //keyValues.Add(1, "mehmet");
+            //foreach (var item in keyValues)
+            //{
+            //    Console.WriteLine(item.Key + ") " + item.Value);
+            //}
+            MyDictionary<int, string> dictionary = new MyDictionary<int, string>();
+            dictionary.Add(1, "Mehmet");
+            dictionary.Add(2, "Feride");
+            Console.WriteLine(dictionary.Count);
+            dictionary.List();
         }
+
+
+      
     }
+
+
+
 }
